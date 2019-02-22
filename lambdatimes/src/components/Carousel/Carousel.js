f1132bc13151be027beb5f40cdf1df0c8a71160a
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-22 12:01:56 +0100

    style: Create styled components and delete stylesheet

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
+import styled from "styled-components"
 // Complete this Carousel 
 export default class Carousel extends Component {
   constructor(props){
@@ -26,10 +27,79 @@ export default class Carousel extends Component {
   
   render(){
     return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
+      <CarouselContainer>
+        <LeftButton onClick={this.leftClick}>{"<"}</LeftButton>
+        <RightButton onClick={this.rightClick}>{">"}</RightButton>
+      </CarouselContainer>
     )
   }
 }
+
+const CarouselContainer = styled.div`
+width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+	height: 500px;
+	position: relative;
+	overflow: hidden;
+  margin-top: 16px;
+  
+  img {
+    width: 100%;
+	display: none;
+  }
+
+@media (min-width: 1200px) {
+		width: 1200px;
+  }
+`
+
+const LeftButton = styled.div`
+  top: 50%;
+	left: 25px;
+	transform: translate(0, -50%);
+  display: flex;
+	justify-content: center;
+	align-items: none;
+	flex-direction: row;
+	color: #fff;
+	background-color: #333;
+	font-size: 40px;
+	border-radius: 50%;
+	position: absolute;
+	width: 50px;
+	height: 50px;
+  cursor: pointer;
+  
+  &:hover {
+    color: #333;
+	  background-color: #fff;
+	  border: 2px solid #333;
+  }
+`
+
+const RightButton = styled.div`
+  top: 50%;
+	right: 25px;
+	transform: translate(0, -50%);
+  display: flex;
+	justify-content: center;
+	align-items: none;
+	flex-direction: row;
+	color: #fff;
+	background-color: #333;
+	font-size: 40px;
+	border-radius: 50%;
+	position: absolute;
+	width: 50px;
+	height: 50px;
+  cursor: pointer;
+  
+  &:hover {
+    color: #333;
+	  background-color: #fff;
+	  border: 2px solid #333;
+  }
+`
